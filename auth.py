7fd1b0754022500529391dd09762809645fb5976
krystivn <krystian50013@gmail.com>
2024-11-06 07:57:17 +0100

    auth.py

--- auth.py
+++ auth.py
@@ -58,4 +58,4 @@ async def auth_google(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=2137)
+    uvicorn.run(app, host="0.0.0.0", port=2137)
